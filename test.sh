1d5c777f354efb75f5657719fa07e9d086350458
Callum Moseley <callum.moseley1@gmail.com>
2020-11-14 14:39:01 -0500

    test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-./robots.py WEC2020/test_cases/case$2.txt -s $1 -o solutions/case$2.out
+python3 robots.py WEC2020/test_cases/case$2.txt -s $1 -o solutions/case$2.out
 mkdir -p visualizations/case$2
 rm -f visualizations/case$2/*
 python WEC2020/visualize_solution.py -p WEC2020/test_cases/case$2.txt -s solutions/case$2.out -o visualizations/case$2
