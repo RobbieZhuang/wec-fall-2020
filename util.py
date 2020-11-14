958079adaddb0a9553f4dfa93d41d3c2bc58fd44
Callum Moseley <callum.moseley1@gmail.com>
2020-11-14 11:13:58 -0500

    boilerplate

---
+++ util.py
@@ -0,0 +1,29 @@
+def equal_space_base_stations(tiles, n):
+    total_len = border_length(tiles)
+
+    placed = 0
+    base_stations = []
+
+    for i, pos in enumerate(border_positions(tiles)):
+        if i / total_len >= placed / n:
+            placed += 1
+            base_stations.append(pos)
+        if placed == n:
+            break
+
+    return base_stations
+
+def border_length(tiles):
+    rows, cols = tiles.shape
+    return rows * 2 + cols * 2
+
+def border_positions(tiles):
+    rows, cols = tiles.shape
+    for i in range(cols):
+        yield (-1, i)
+    for i in range(rows):
+        yield (i, cols)
+    for i in range(cols):
+        yield (rows, cols - i - 1)
+    for i in range(rows):
+        yield (rows - i - 1, -1)
